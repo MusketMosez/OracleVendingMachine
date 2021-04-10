71300e404d13fe70dd2dbb9394a47a14fdb765ad
Nathan Moses <nathanmoses13@gmail.com>
2021-04-10 13:04:04 +0100

    added template api call to initialise vending machine

--- main.py
+++ main.py
@@ -4,7 +4,32 @@ import argparse
 import sys
 import os
 
-# Create Parser
-myParser = argparse.ArgumentParser(description='Operate Vending Machine')
 
-args = myParser.parse_args()
+# dictionary (hash table to store change)
+
+
+
+# API functions
+def init_vending_machine():
+    if len(sys.argv) < 3:
+        print(InitVendParser.print_usage())
+
+# Create Parser
+parser = argparse.ArgumentParser(prog='vend',
+                                 description='operate Vending Machine',
+                                 )
+
+# add subparsers for sub commands
+subparsers = parser.add_subparsers()
+
+InitVendParser = subparsers.add_parser('init',
+                                       help='initialise vending machine with float value',
+                                       usage='init [float]')
+InitVendParser.add_argument('float', type=float)
+InitVendParser.set_defaults(func=init_vending_machine)
+
+if len(sys.argv) <= 1:
+    sys.argv.append('--help')
+
+args = parser.parse_args()
+args.func()
