095497fa31714ac13dba06f535ee910a61fee43a
Nathan Moses <nathanmoses13@gmail.com>
2021-04-10 15:38:35 +0100

    added calls to initialise and print float

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 from cmd import Cmd
 import sys
 
-floatChange = 0.0
+
 
 
 class CmdSubclass(Cmd):
 
-    def do_init(self, arg):
-        """ Command to initialise"""
-        global floatChange
-        floatChange = float(arg)
+    def __init__(self):
+        self.floatChange = 0.0
+        self.currentCost = 0.0
+        self.hasSelected = False
+        self.hasPurchased = False
+        self.userCoins = {}
+        super(CmdSubclass, self).__init__()
 
-    def do_printfloat(*args):
-        """Help text for export"""
-        print(floatChange)
+    def do_init(self, arg):
+        """ Command to initialise float amount for vending machine\n Usage: init [float]"""
+        usage = 'Usage: init [float]'
+
+        try:
+            self.floatChange = float(arg)
+        except ValueError:
+            print("ValueError: Initialise vending machine with float value")
+            print(usage)
+
+    def do_printfloat(self, arg):
+        """Command to print current vending machine float value\nUsage: pc"""
+        usage = 'Usage: pc'
+        if arg:
+            print("Argument not required")
+            print(usage)
+        print(self.floatChange)
+
+    def do_cost(self, arg):
+
+        try:
+            self.currentCost += arg
+        except ValueError:
+            print('ValueError: Enter float value as cost of item')
+        self.hasSelected = True
+
+    def do_buy(self, arg):
+        if self.hasSelected == True and self.currentCost > 0.0:
+            try:
+                self.userCoins = parse_coins(arg)
+
+    def do_test(self, arg):
+        print(type(arg.split(' ')))
+
+
+    # def do_purchase(self, arg):
 
     def do_exit(*args):
         return -1
