5582ddccfa5747f4057be3943d40b20484a78272
Nathan Moses <nathanmoses13@gmail.com>
2021-04-11 14:07:51 +0100

    added help messages and usage for all functions

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import sys
 
 
 # helper functions
+
+# changes sum of money into change
 def parse_coins(coins, userCoins):
 
+    # separate pounds from change
     pounds = math.floor(coins)
     smallChange = coins - pounds
 
+    # add the number of £1 and £2 coins to dictionary
     if pounds != 0:
         if pounds == 1:
             userCoins['1'] += 1
@@ -19,6 +23,9 @@ def parse_coins(coins, userCoins):
                 userCoins['1'] += 1
                 userCoins['2'] += (pounds - 1) / 2
 
+
+
+    # add the number of 50p, 20p, 10p, 5p and 1p to dictionary
     fifties, smallChange = divmod(smallChange, 0.50)
     twenties, smallChange = divmod(smallChange, 0.20)
     tens, smallChange = divmod(smallChange, 0.10)
@@ -34,6 +41,7 @@ def parse_coins(coins, userCoins):
     return userCoins
 
 
+# prints dictionary of coins in readable format
 def print_coins(coins):
     for key in coins.keys():
         if coins[key] != 0:
@@ -46,6 +54,7 @@ def print_coins(coins):
                 print('£' + key + ' X ' + str(int(coins[key])))
 
 
+# class contains call functions for API
 class CmdSubclass(Cmd):
 
     def __init__(self):
@@ -71,6 +80,8 @@ class CmdSubclass(Cmd):
             print(usage)
 
     def do_buy(self, arg):
+        """Command to purchase item for a specified price\n Usage: buy [float]"""
+        usage = 'Usage: buy [float]'
         if self.floatChange <= 0:
             print('Insert more float change using the init command')
         else:
@@ -80,10 +91,13 @@ class CmdSubclass(Cmd):
                     raise ValueError
             except ValueError:
                 print('ValueError: Enter positive float value as cost of item')
+                print(usage)
             else:
                 self.hasSelected = True
 
     def do_deposit(self, arg):
+        """Command to purchase item for a specified price\n Usage: buy [float]"""
+        usage = 'Usage: buy [float]'
         if self.floatChange <= 0:
             print('Insert more float change using the init command')
         else:
@@ -94,6 +108,7 @@ class CmdSubclass(Cmd):
                         raise ValueError
                 except ValueError:
                     print('ValueError: Deposit positive float value')
+                    print(usage)
                 else:
                     self.userCoins = parse_coins(float(arg), self.userCoins)
                     print('Current cost of selection: ' + "£{:,.2f}".format(self.currentCost))
@@ -114,7 +129,7 @@ class CmdSubclass(Cmd):
                 print('Make a selection using buy command')
 
     def do_getfloat(self, arg):
-        """Command to print current vending machine float value\nUsage: pc"""
+        """Command to print current vending machine float amount\nUsage: getfloat"""
         usage = 'Usage: getfloat'
         if arg:
             print("Argument not required")
@@ -122,31 +137,41 @@ class CmdSubclass(Cmd):
         print("£{:,.2f}".format(self.floatChange))
 
     def do_getchange(self,arg):
+        """"Command to receive change due\nUsage: getchange"""
+        usage = 'Usage :getchange'
+        if arg:
+            print("Argument not required")
+            print(usage)
         print_coins(self.changeDue)
 
     def do_getdeposit(self, arg):
-        print("£{:,.2f}".format(self.paidAmount))
-
-    def do_getdepositcoins(self, arg):
-        print_coins(self.userCoins)
+        """Command to receive the total amount currently\ndeposited by user with option to recieve amount in change format\nUsage: getdeposit {change} """
+        usage = 'Usage: getdeposit {change}'
+        if arg:
+            if arg == 'change':
+                print_coins(self.userCoins)
+            else:
+                print(' To receive amount in change format enter: "getdeposit change" ')
+        else:
+            print("£{:,.2f}".format(self.paidAmount))
 
     def do_getcost(self, arg):
+        """Command to receive cumulative cost of all selections\nUsage: getcost """
+        usage = 'Usage: getcost'
+        if arg:
+            print("Argument not required")
+            print(usage)
         print("£{:,.2f}".format(self.currentCost))
-
 
     def do_test(self, arg):
         print(type(arg.split(' ')))
-
-
-    # def do_purchase(self, arg):
 
     def do_exit(*args):
         return -1
 
 
 if __name__ == '__main__':
-    temp_dict = {'2':0, '1': 0,'0.50': 0, '0.20': 0, '0.10': 0, '0.05': 0, '0.01': 0}
-    parse_coins(12.77, temp_dict)
+
     c = CmdSubclass()
     command = ' '.join(sys.argv[1:])
     if command:
